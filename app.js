936cb19b7346a1efec91766b22c885628c5363e7
Dashpeljee37 <dashpeljee37@gmail.com>
2021-12-25 23:51:57 +0800

    dice roll button function

--- app.js
+++ app.js
@@ -4,31 +4,20 @@ var score = [0, 0];
 
 var roundScore = 0;
 
-var dice = Math.round(Math.random() * 5) + 1;
-console.log(dice);
+var diceNumber = Math.round(Math.random() * 5) + 1;
+console.log(diceNumber);
 
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
+document.getElementById("score-0").textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 document.querySelector(".dice").style.display =  'none';
+var diceDom = document.querySelector(".dice")
 
-switch(dice){
-    case 1:
-        document.querySelector(".dice").src = 'dice-1.png'; break;
-    case 2:
-        document.querySelector(".dice").src = 'dice-2.png'; break;
-    case 3:
-        document.querySelector(".dice").src = 'dice-3.png'; break;
-    case 4:
-        document.querySelector(".dice").src = 'dice-4.png'; break;
-    case 5:
-        document.querySelector(".dice").src = 'dice-5.png'; break;
-    case 6:
-        document.querySelector(".dice").src = 'dice-6.png'; break;
-    default:
-        document.querySelector(".dice").style.display = 'none';
-}
+document.querySelector(".btn-roll").addEventListener("click", function (){
+    var diceNumber = Math.round(Math.random() * 5) + 1;
+    diceDom.style.display = 'block';
+    diceDom.src = "dice-" + diceNumber + ".png";
+});
 
-console.log("fuck")
 // document.querySelector('#score-1').innerHTML = '<em> YES! </em>'
